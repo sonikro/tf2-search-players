779f1f3e3ddee890f97a94765cde1dba5108ed54
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2021-03-31 22:46:49 -0300

    feat(JSONServerRepository): made JSONServerRepository dinamically generate all Servers from Brazil

--- src/entrypoints/index.ts
+++ src/entrypoints/index.ts
@@ -1,6 +1,27 @@
-import query from "source-server-query";
+import prompt from 'prompt';
+import { findServerByPlayerName } from '../core/usecases/findServerByPlayerName';
+import { JSONServerRepository } from '../providers/JSONServerRepository';
+import { PinoLogger } from '../providers/PinoLogger';
+import { SourceServerQuery } from '../providers/SourceServerQuery';
 
-query
-    .players("155.133.224.162", 27069, 2000)
-    .then(console.log)
-    .catch(console.log);
+
+const dependencies = {
+    logService: new PinoLogger(),
+    serverRepository: new JSONServerRepository(),
+    serverQueryService: new SourceServerQuery(2000)
+}
+prompt.start();
+
+prompt.get(['nickname',], function (err, result) {
+    if (err) { return onErr(err); }
+
+    findServerByPlayerName(dependencies)(result.nickname as string)
+        .then(servers => console.log(servers))
+        .then(() => process.exit(0))
+        .catch(onErr)
+});
+
+function onErr(err: Error) {
+    console.error(err);
+    return 1;
+}
--- src/providers/JSONServerRepository.ts
+++ src/providers/JSONServerRepository.ts
@@ -2,17 +2,30 @@ import { Server } from "../core/domain/Server";
 import { ServerRepository } from "../core/providers/ServerRepository";
 import servers from "../../servers.json";
 
+interface ServerEntry {
+    [ip: string]: {
+        from: number;
+        to: number;
+    }
+}
 /**
  * This repository will read from the servers.json file in the root of the project
  */
 export class JSONServerRepository implements ServerRepository {
     async getServers(): Promise<Server[]> {
-        return servers.map(serverString => {
-            const splitted = serverString.split(":");
-            return {
-                ip: splitted[0],
-                port: parseInt(splitted[1], 10)
-            } as Server
-        })
+        const serverData = servers as ServerEntry
+        const ips = Object.keys(servers);
+        const generatedServers: Server[] = []
+
+        for (const ip of ips) {
+            const portRange = serverData[ip];
+            for (let port = portRange.from; port <= portRange.to; port++) {
+                generatedServers.push({
+                    ip,
+                    port
+                })
+            }
+        }
+        return generatedServers;
     }
 }
